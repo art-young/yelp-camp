9600c897ba0de6e9f6804388ace9d1e1e9e5a524
Art <art1121@gmail.com>
2020-01-14 19:43:18 +0000

    Add environment variable for database url

--- app.js
+++ app.js
@@ -22,11 +22,10 @@ mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
 
-// Connect to local yelp_camp mongo db
-// mongoose.connect("mongodb://localhost:27017/yelp_camp");
-
-// MongoDB Atlas free cluster connection string
-mongoose.connect("mongodb+srv://art:dbUserPasswordart@cluster0-ihv0i.mongodb.net/test?retryWrites=true&w=majority");
+// Connect to MongoDB server
+// dev: mongodb://localhost:27017/yelp_camp
+// prod: mongodb+srv://art:dbUserPasswordart@cluster0-ihv0i.mongodb.net/test?retryWrites=true&w=majority
+mongoose.connect(process.env.DATABASEURL);
 
 // Need this line to properly use body-parser
 app.use(bodyParser.urlencoded({extended: true}));
